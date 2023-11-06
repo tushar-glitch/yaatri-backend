7f1eb80c31b4607ca20dc62a213ad9c70bf85a91
tushar-glitch <tusharc20001@gmail.com>
2023-11-06 22:09:57 +0530

    mongodb link updated

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
+require("dotenv").config()
 const cors = require('cors')
 const PORT = process.env.port || 3001;
 const mongoose = require('mongoose')
 const user_route = require('./routes/user_route')
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb+srv://tushar-glitch:rdpj8rWs6HOegE9o@cluster0.fjkozqb.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(`mongodb+srv://tushar-glitch:${process.env.MongoPass}@cluster0.fjkozqb.mongodb.net/?retryWrites=true&w=majority`)
 app.use(express.json())
 app.use(cors())
 const db = mongoose.connection;
